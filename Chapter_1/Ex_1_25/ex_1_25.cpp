2eb21dc00bf43b00612c4a4397ab3852dc3f3a58
Hao Wu <haowu199368@gmail.com>
2018-01-15 20:53:13 -0600

    Exercise 1.25

---
+++ Chapter_1/Ex_1_25/ex_1_25.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include"Sales_item.h"
+
+int main()
+{
+	Sales_item total;
+	if(std::cin >> total){
+		Sales_item trans;
+		while(std::cin >> trans){
+			if(total.isbn() == trans.isbn()){
+				total += trans;
+			}else{
+				std::cout << total << std::endl;
+				total = trans;
+			}
+		}
+
+		std::cout << total << std::endl;
+	}else{
+		std::cout << "Your input is wrong" << std::endl;
+	}
+
+	return 0;
+}
